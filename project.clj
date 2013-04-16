71e85cc9a0fb8d1b96bc3376bfac7e42463e6667
Alexander Solovyov <alexander@solovyov.net>
2013-04-16 17:32:19 +0300

    finally working

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
-  :plugins      [[lein-hlisp "1.0.0"]]
+  :plugins      [[lein-hlisp "1.0.0" :exclusions [org.clojure/clojurescript]]]
   :dependencies [[hlisp-macros "1.0.0"]
                  [hlisp-util "0.1.0-SNAPSHOT"]
-                 [hlisp-reactive "1.0.0-SNAPSHOT"]
+                 [hlisp-reactive "1.0.2-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-1586"]
                  [alandipert/storage-atom "1.1.1"]]
   :eval-in      :leiningen
   :hlisp        {:html-src    "src/html"
